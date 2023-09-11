1cd40a3fb52a64bc228fe284e9e95ebdd2d97a69
jerem06 <jeremie.bencini@gmail.com>
2023-09-11 10:39:57 +0200

    fix(craousel) : fix carousel responsi

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -12,19 +12,14 @@ const slides = [
   {
     url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
   },
-
-  {
-    url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
-  },
 ];
 
 export default function Education() {
   return (
     <div className="pt-24 flex flex-col items-center">
-      <Text>Un parcours élitiste</Text>
+      <Text variant="body" className="text-2xl text-headline py-5">
+        Un parcours élitiste
+      </Text>
       <Carousel data={slides} />
       <Text>Un parcours élitiste</Text>
     </div>
--- src/components/organisms/Carousel/Carousel.tsx
+++ src/components/organisms/Carousel/Carousel.tsx
@@ -1,37 +1,70 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ICarouselProps } from "./Carousel.props";
 import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 
 export const Carousel = ({ data, ...props }: ICarouselProps) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  const prevSlide = () => {
+    if (currentIndex === 0) setCurrentIndex(data.length - 1);
+    setCurrentIndex((current) => current - 1);
+  };
+
+  const nextSlide = () => {
+    if (currentIndex === data.length - 1) setCurrentIndex(0);
+    setCurrentIndex((current) => current + 1);
+  };
+
+  const handleIndexToDisplay = (relativeIndex: number) => {
+    let indexToDisplay = relativeIndex;
+    if (relativeIndex === -1) {
+      indexToDisplay = data.length - 1;
+    }
+    if (relativeIndex === data.length) {
+      indexToDisplay = 0;
+    }
+    return indexToDisplay;
+  };
 
   return (
-    <>
-      <div className="absolute z-20 -left-2 text-primary-900 lg:top-[48%] top-[46%]">
+    <div className="relative w-full flex justify-between items-center">
+      <div onClick={prevSlide} className="z-10 -ml-2 text-primary-900">
         <IoChevronBackOutline size={50} />
       </div>
-      <div className="hidden sm:block absolute lg:max-w-xl max-w-lg  w-full top-[25%] px-4 left-0">
+      <div className="hidden sm:block absolute lg:max-w-xl max-w-lg  w-full lg:top-[8%] top-[6%] px-4 left-0">
         <div
-          style={{ backgroundImage: `url(${data[currentIndex + 1].url})` }}
+          style={{
+            backgroundImage: `url(${
+              data[handleIndexToDisplay(currentIndex - 1)].url
+            })`,
+          }}
           className="aspect-[4/3] rounded-2xl bg-center bg-cover duration-500"
         ></div>
       </div>
-      <div className="lg:max-w-2xl max-w-xl w-full lg:py-8 py-10 px-4 z-10">
+      <div className="lg:max-w-2xl max-w-xl w-full spx-4 z-10">
         <div
-          style={{ backgroundImage: `url(${data[currentIndex].url})` }}
+          style={{
+            backgroundImage: `url(${
+              data[handleIndexToDisplay(currentIndex)].url
+            })`,
+          }}
           className="aspect-[4/3] rounded-2xl bg-center bg-cover duration-500"
         ></div>
       </div>
-      <div className="hidden sm:block absolute lg:max-w-xl max-w-lg  w-full top-[25%] px-4 right-0">
+      <div className="hidden sm:block absolute lg:max-w-xl max-w-lg  w-full lg:top-[8%] top-[6%] px-4 right-0">
         <div
-          style={{ backgroundImage: `url(${data[currentIndex + 2].url})` }}
+          style={{
+            backgroundImage: `url(${
+              data[handleIndexToDisplay(currentIndex + 1)].url
+            })`,
+          }}
           className="aspect-[4/3] rounded-2xl bg-center bg-cover duration-500"
         ></div>
       </div>
-      <div className="absolute z-20 -right-2 text-primary-900 lg:top-[48%] top-[46%]">
+      <div onClick={nextSlide} className="z-10 -mr-2 text-primary-900">
         <IoChevronForwardOutline size={50} />
       </div>
-    </>
+    </div>
   );
 };
